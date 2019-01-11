4be713aefc2e03b901e5378eed52add9bb5f03d1
Ming Xu <920972751@qq.com>
2019-01-11 10:30:33 +0800

    update

---
+++ test/express.cpp
@@ -0,0 +1,16 @@
+// express.cpp values of expressions
+
+# include <iostream>
+int main()
+{
+    using namespace std;
+    int x;
+
+    cout <<  "The expression x = 100 has the value ";
+    cout << (x = 100) << endl;
+
+    cout << "Now x  = " << x << endl;
+
+    cout << "The expression x < 3 has the value ";
+    
+}
